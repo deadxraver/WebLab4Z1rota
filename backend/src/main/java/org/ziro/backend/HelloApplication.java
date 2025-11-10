a4054e4b8d818d28611aae0ca11d48b0ac62b8de
Ziro <Global_counter@mail.ru>
2025-11-10 11:50:44 +0300

    started work with lab

---
+++ backend/src/main/java/org/ziro/backend/HelloApplication.java
@@ -0,0 +1,13 @@
+package org.ziro.backend;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+    public HelloApplication() {
+        super();
+    }
+
+}
