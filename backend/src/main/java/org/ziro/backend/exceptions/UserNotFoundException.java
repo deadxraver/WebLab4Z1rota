d99198a7865e572a0da6bdc6d501728780ebd26a
deadxraver <deadxraver@yandex.ru>
2025-12-04 12:04:38 +0300

    remove redundant + update .gitignore

--- backend/src/main/java/org/ziro/backend/exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.ziro.backend.exceptions;
-
-import jakarta.ws.rs.core.Response;
-
-public class UserNotFoundException extends ApplicationException {
-    public UserNotFoundException(Response.Status status, String message) {
-        super(status,message);
-    }
-}
